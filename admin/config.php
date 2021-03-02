1bae78471aba6258e4f2b194e3edc7d07994397f
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-03 00:33:07 +0600

    comment added

--- admin/config.php
+++ admin/config.php
@@ -1,4 +1,6 @@
 <?php
     $hostname ="http://localhost/news-site";
+
+    // first host name, secent database username, thard dataqbase password, last database name
     $conn = mysqli_connect("localhost","root", "mysql", "news") or die("Connetion failed : " .mysqli_connect_error());
 ?>
