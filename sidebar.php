62d07419863a1e87113d4262313a2c963ddf75e8
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-02 22:06:16 +0600

    sidebar facebook page add

--- sidebar.php
+++ sidebar.php
@@ -102,4 +102,10 @@
         </div>
     </div>
     <!-- /recent posts box -->
+    <!-- facebook page stard -->
+    <div class="search-box-container">
+        <h4>Facebook</h4>
+        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fraihan901&tabs=timeline&width=340&height=300&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=true&appId=298974897931071" width="340" height="300" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+    </div>
+    <!-- facebook page End -->
 </div>
