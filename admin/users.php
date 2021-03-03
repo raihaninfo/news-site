11e7c40c12f3d785d70c98b35879f654c6c56cdd
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-03 22:01:13 +0600

    user pagination add

--- admin/users.php
+++ admin/users.php
@@ -11,7 +11,16 @@
               <div class="col-md-12">
               <?php
                 include"config.php";
-                $sql = "SELECT * FROM user ORDER BY user_id DESC";
+                $limit =3;
+                
+                if(isset($_GET['page'])){
+                    $page = $_GET['page'];
+                }else{
+                    $page = 1;
+                }
+                $ofset = ($page -1) * $limit;
+
+                $sql = "SELECT * FROM user ORDER BY user_id DESC LIMIT {$ofset},{$limit}";
                 $result = mysqli_query($conn, $sql) or die("Query Failed.");
                 if(mysqli_num_rows($result) > 0){
               ?>
@@ -48,12 +57,30 @@
                   </table>
                 <?php  
             } 
+                
+                $sql1 = "SELECT * FROM user";
+                $result1 = mysqli_query($conn, $sql1) or die("Query Failed.");
+
+                if(mysqli_num_rows($result1) > 0){
+                    $total_records = mysqli_num_rows($result1);
+                    $total_page = ceil($total_records /$limit);
+                    echo '<ul class="pagination admin-pagination">';
+                    for($i =1; $i <= $total_page; $i++){
+                        if($i ==$page){
+                            $active = "active";
+                        }else{
+                            $active = "";
+                        }
+                        echo '<li class="'.$active.'"><a href="users.php?page='.$i.'">'.$i.'</a></li>';
+                    }
+                    echo '</ul>';
+                }
+
                 ?>
-                  <ul class='pagination admin-pagination'>
-                      <li class="active"><a>1</a></li>
-                      <li><a>2</a></li>
-                      <li><a>3</a></li>
-                  </ul>
+                  
+                      <!-- <li class="active"><a>1</a></li> -->
+                      
+                  
               </div>
           </div>
       </div>
