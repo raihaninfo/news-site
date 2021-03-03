c979b4441c481cf435bdb65bbfbc0cb2fec87cfe
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-03 23:32:42 +0600

    login system add

--- admin/index.php
+++ admin/index.php
@@ -18,7 +18,7 @@
                         <img class="logo" src="images/news.jpg">
                         <h3 class="heading">Admin</h3>
                         <!-- Form Start -->
-                        <form  action="" method ="POST">
+                        <form  action="<?php $_SERVER['PHP_SELF'];?>" method ="POST">
                             <div class="form-group">
                                 <label>Username</label>
                                 <input type="text" name="username" class="form-control" placeholder="" required>
@@ -30,6 +30,29 @@
                             <input type="submit" name="login" class="btn btn-primary" value="login" />
                         </form>
                         <!-- /Form  End -->
+                        <?php
+                            if(isset($_POST['login'])){
+                                include"config.php";
+                                $usrname = mysqli_real_escape_string($conn, $_POST['username']);
+                                $password = md5($_POST['password']);
+
+                                $sql = "SELECT user_id, username, role FROM user WHERE username = '{$usrname}' AND password ='{$password}'";
+                                $result = mysqli_query($conn, $sql) or die("Query Failed.");
+
+                                if(mysqli_num_rows($result) > 0){
+                                    while($row = mysqli_fetch_assoc($result)){
+                                        session_start();
+                                        $_SESSION["username"] = $row['username'];
+                                        $_SESSION["user_id"] = $row['user_id'];
+                                        $_SESSION["user_role"] = $row['role'];
+
+                                        header("Location: {$hostname}/admin/post.php");
+                                    }
+                                }else{
+                                    echo '<div class="alert alert-danger">Username and password not match.</div>';
+                                }
+                            }
+                        ?>
                     </div>
                 </div>
             </div>
