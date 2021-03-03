411eb8048f54d18f254e82a27ed99f55b4e61712
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-03 12:02:50 +0600

    user edit option add

--- admin/users.php
+++ admin/users.php
@@ -40,8 +40,8 @@
                                     echo"Normal";
                                 }
                               ?></td>
-                              <td class='edit'><a href='update-user.php'><i class='fa fa-edit'></i></a></td>
-                              <td class='delete'><a href='delete-user.php'><i class='fa fa-trash-o'></i></a></td>
+                              <td class='edit'><a href='update-user.php?id=<?php echo $row["user_id"]; ?>'><i class='fa fa-edit'></i></a></td>
+                              <td class='delete'><a href='delete-user.php?id=<?php echo $row["user_id"]; ?>'><i class='fa fa-trash-o'></i></a></td>
                           </tr>
                           <?php } ?>
                       </tbody>
