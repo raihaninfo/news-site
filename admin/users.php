0e3e4ea344226ea6183c644fa689bbe0e9edb7a2
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-03 22:49:57 +0600

    user section complete

--- admin/users.php
+++ admin/users.php
@@ -65,6 +65,10 @@
                     $total_records = mysqli_num_rows($result1);
                     $total_page = ceil($total_records /$limit);
                     echo '<ul class="pagination admin-pagination">';
+                    if($page >1){
+                        echo '<li><a href="users.php?page='.($page -1).'">Prev</a></li>';
+                    }
+                    
                     for($i =1; $i <= $total_page; $i++){
                         if($i ==$page){
                             $active = "active";
@@ -72,6 +76,9 @@
                             $active = "";
                         }
                         echo '<li class="'.$active.'"><a href="users.php?page='.$i.'">'.$i.'</a></li>';
+                    }
+                    if($total_page > $page){
+                      echo '<li><a href="users.php?page='.($page +1).'">Next</a></li>';  
                     }
                     echo '</ul>';
                 }
