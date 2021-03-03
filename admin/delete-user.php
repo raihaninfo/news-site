a51c58d8b93a40f53eff884651a65037ecac5e57
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-03 12:17:55 +0600

    user delete option add

---
+++ admin/delete-user.php
@@ -0,0 +1,14 @@
+<?php
+    include"config.php";
+
+    $userid = $_GET['id'];
+
+    $sql = "DELETE FROM user WHERE user_id ={$userid}";
+
+    if (mysqli_query($conn, $sql)) {
+        header("Location: {$hostname}/admin/users.php");
+    }else{
+        echo "Can\'t Delete the User Record.";
+    }
+    mysqli_close($conn);
+?>
