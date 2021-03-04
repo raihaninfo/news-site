9ad080969245be992b8cc4934fa8e284c756e05a
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-04 12:29:05 +0600

    add post error fix

--- admin/add-post.php
+++ admin/add-post.php
@@ -26,7 +26,7 @@
                             $result = mysqli_query($conn, $sql) or die("Query Failed.");
                             if (mysqli_num_rows($result) > 0) {
                                 while($row = mysqli_fetch_assoc($result)){
-                                    echo "<option>{$row['category_name']}</option>"
+                                    echo "<option>{$row['category_name']}</option>";
                                 }
 
                             }
