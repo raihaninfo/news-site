2477f15d80d0d6eb27d8d7feddd63657750c22fe
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-04 12:26:17 +0600

    category delete option aded

--- admin/delete-category.php
+++ admin/delete-category.php
@@ -1,11 +1,15 @@
 <?php
-if($_SESSION['user_role']=="0"){
-    header("Location: {$hostname}/admin/post.php");
-}
+    include"config.php";
+    if($_SESSION['user_role']=="0"){
+        header("Location: {$hostname}/admin/post.php");
+    }
+    $catid = $_GET['id'];
 
+    $sql = "DELETE FROM category WHERE category_id ={$catid}";
 
-
-
-
-
-?>
+    if (mysqli_query($conn, $sql)) {
+        header("Location: {$hostname}/admin/category.php");
+    }else{
+        echo "Can\'t Delete the Category Record.";
+    }
+    mysqli_close($conn);
