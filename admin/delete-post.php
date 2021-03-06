e608338557ed8cfc5553c4e8fe8c5ba409adbda2
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-07 01:32:15 +0600

    delete post code format

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -1,17 +1,17 @@
 <?php
-    include"config.php";
-    // if($_SESSION['user_role']=="0"){
-    //     header("Location: {$hostname}/admin/post.php");
-    // }
-    $postid = $_GET['id'];
-    $cat_id = $_GET['catid'];
+include "config.php";
+// if($_SESSION['user_role']=="0"){
+//     header("Location: {$hostname}/admin/post.php");
+// }
+$postid = $_GET['id'];
+$cat_id = $_GET['catid'];
 
-    $sql = "DELETE FROM post WHERE post_id ={$postid};";
-    $sql .="UPDATE category SET post = post-1 WHERE category_id={$cat_id}";
+$sql = "DELETE FROM post WHERE post_id ={$postid};";
+$sql .= "UPDATE category SET post = post-1 WHERE category_id={$cat_id}";
 
-    if (mysqli_multi_query($conn, $sql)) {
-        header("Location: {$hostname}/admin/post.php");
-    }else{
-        echo "Query Faild.";
-    }
-    mysqli_close($conn);
+if (mysqli_multi_query($conn, $sql)) {
+    header("Location: {$hostname}/admin/post.php");
+} else {
+    echo "Query Faild.";
+}
+mysqli_close($conn);
