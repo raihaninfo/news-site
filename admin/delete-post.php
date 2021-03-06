42cdba813d810ff86ce259255b455a8cff2332de
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-07 01:24:16 +0600

    admin panel complete

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -1,15 +1,17 @@
 <?php
     include"config.php";
-    if($_SESSION['user_role']=="0"){
-        header("Location: {$hostname}/admin/post.php");
-    }
+    // if($_SESSION['user_role']=="0"){
+    //     header("Location: {$hostname}/admin/post.php");
+    // }
     $postid = $_GET['id'];
+    $cat_id = $_GET['catid'];
 
-    $sql = "DELETE FROM post WHERE post_id ={$postid}";
+    $sql = "DELETE FROM post WHERE post_id ={$postid};";
+    $sql .="UPDATE category SET post = post-1 WHERE category_id={$cat_id}";
 
-    if (mysqli_query($conn, $sql)) {
+    if (mysqli_multi_query($conn, $sql)) {
         header("Location: {$hostname}/admin/post.php");
     }else{
-        echo "Can\'t Delete the Category Record.";
+        echo "Query Faild.";
     }
     mysqli_close($conn);
--- admin/post.php
+++ admin/post.php
@@ -21,12 +21,12 @@
                 $ofset = ($page - 1) * $limit;
 
                 if ($_SESSION['user_role'] == "1") {
-                    $sql = "SELECT post.post_id, post.title, post.description, post.post_date, category.category_name, user.username FROM post 
+                    $sql = "SELECT post.post_id, post.title, post.description, post.post_date, category.category_name, user.username, post.category FROM post 
                     LEFT JOIN category ON post.category = category.category_id
                     LEFT JOIN user ON post.author = user.user_id
                     ORDER BY post.post_id DESC LIMIT {$ofset},{$limit}";
                 } elseif ($_SESSION['user_role'] == "0") {
-                    $sql = "SELECT post.post_id, post.title, post.description, post.post_date, category.category_name, user.username FROM post 
+                    $sql = "SELECT post.post_id, post.title, post.description, post.post_date, category.category_name, user.username, post.category FROM post 
                     LEFT JOIN category ON post.category = category.category_id
                     LEFT JOIN user ON post.author = user.user_id
                     WHERE post.author = {$_SESSION['user_id']}
@@ -56,7 +56,7 @@
                                     <td><?php echo $row['post_date']; ?></td>
                                     <td><?php echo $row['username']; ?></td>
                                     <td class='edit'><a href='update-post.php?id=<?php echo $row['post_id']; ?>'><i class='fa fa-edit'></i></a></td>
-                                    <td class='delete'><a href='delete-post.php?id=<?php echo $row['post_id']; ?>'><i class='fa fa-trash-o'></i></a></td>
+                                    <td class='delete'><a href='delete-post.php?id=<?php echo $row['post_id']; ?>&catid=<?php echo $row['category']; ?>'><i class='fa fa-trash-o'></i></a></td>
                                 </tr>
                             <?php } ?>
                         </tbody>
