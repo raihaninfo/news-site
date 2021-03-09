029e0632ceebca4ec9b684adf50bace848f03208
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-09 11:47:16 +0600

    dynamic year aded

--- admin/add-user.php
+++ admin/add-user.php
@@ -1,5 +1,5 @@
-<?php include "header.php"; 
-if($_SESSION['user_role']=="0"){
+<?php include "header.php";
+if ($_SESSION['user_role'] == "0") {
     header("Location: {$hostname}/admin/post.php");
 }
 ?>
--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <span>© Copyright 2019 News | Powered by <a style="text-decoration: none;"href="https://raihan-cv.netlify.app/">Raihan</a></span>
+                <span>© Copyright <?php echo date("Y")?> News | Powered by <a style="text-decoration: none;"href="https://raihan-cv.netlify.app/">Raihan</a></span>
             </div>
         </div>
     </div>
