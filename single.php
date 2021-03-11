7baae0c9913e783c187eabace36d2959fd6a3557
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-11 15:45:51 +0600

    single post page complete

--- single.php
+++ single.php
@@ -5,27 +5,47 @@
             <div class="col-md-8">
                 <!-- post-container -->
                 <div class="post-container">
+                <?php
+                include "config.php";
+
+                $post_id = $_GET['id'];
+
+                $sql = "SELECT post.post_id, post.title, post.description, post.post_date, category.category_name, user.username, post.category, post.post_img FROM post 
+                    LEFT JOIN category ON post.category = category.category_id
+                    LEFT JOIN user ON post.author = user.user_id
+                    WHERE post.post_id = {$post_id}";
+
+                $result = mysqli_query($conn, $sql) or die("Query Failed.");
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                ?>
                     <div class="post-content single-post">
-                        <h3>Lorem ipsum dolor sit amet, consectetur</h3>
+                        <h3><?php echo $row['title'] ?></h3>
                         <div class="post-information">
                             <span>
                                 <i class="fa fa-tags" aria-hidden="true"></i>
-                                Html
+                                <?php echo $row['category_name'] ?>
                             </span>
                             <span>
                                 <i class="fa fa-user" aria-hidden="true"></i>
-                                <a href='author.php'>Admin</a>
+                                <a href='author.php'><?php echo $row['username'] ?></a>
                             </span>
                             <span>
                                 <i class="fa fa-calendar" aria-hidden="true"></i>
-                                01 Nov, 2019
+                                <?php echo $row['post_date'] ?>
                             </span>
                         </div>
-                        <img class="single-feature-image" src="images/post_1.jpg" alt="" />
+                        <img class="single-feature-image" src="admin/upload/<?php echo $row['post_img'] ?>" alt="" />
                         <p class="description">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                        <?php echo $row['description'] ?>
                         </p>
                     </div>
+                    <?php
+                        }
+                    } else {
+                        echo "<h2>No Record Found.<h2/>";
+                    }
+                            ?>
                 </div>
                 <!-- /post-container -->
             </div>
