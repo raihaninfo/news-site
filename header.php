d9b3be7ef905abedee7f755588eb3fd1ea34089a
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-11 22:20:50 +0600

    white space remove

--- header.php
+++ header.php
@@ -36,35 +36,32 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                <?php 
-                    include"config.php";
-                    if(isset($_GET['cid'])){
+                    <?php
+                    include "config.php";
+                    if (isset($_GET['cid'])) {
                         $cat_id = $_GET['cid'];
                     }
 
-                    
-
                     $sql = "SELECT * FROM category  WHERE post > 0";
-                    
+
                     $result = mysqli_query($conn, $sql) or die("Query Failed. : Category");
-                    if(mysqli_num_rows($result) > 0){
+                    if (mysqli_num_rows($result) > 0) {
                         $active = "";
-                      
-                ?>
-                    <ul class='menu'>
-                    <?php while($row = mysqli_fetch_assoc($result)) {
-                        if (isset($_GET['cid'])) {
-                            if ($row['category_id'] == $cat_id) {
-                                $active = "active";
-                            } else {
-                                $active = "";
-                            }
-                        }
-                      
-                      
-                      echo  "<li><a class='{$active}' href='category.php?cid={$row['category_id']}'>{$row['category_name']}</a></li>";
-                     } ?>
-                    </ul>
+
+                    ?>
+                        <ul class='menu'>
+                            <?php while ($row = mysqli_fetch_assoc($result)) {
+                                if (isset($_GET['cid'])) {
+                                    if ($row['category_id'] == $cat_id) {
+                                        $active = "active";
+                                    } else {
+                                        $active = "";
+                                    }
+                                }
+
+                                echo  "<li><a class='{$active}' href='category.php?cid={$row['category_id']}'>{$row['category_name']}</a></li>";
+                            } ?>
+                        </ul>
                     <?php } ?>
                 </div>
             </div>
